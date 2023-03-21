98d72c7a813030f89a8a37860bb87c9582ab2abc
Louisgit14 <louis.akurathi14@gmail.com>
2023-03-21 17:33:44 -0500

    initial commit Homework problems

---
+++ isLessThan30.js
@@ -0,0 +1,11 @@
+function isLessThan30(number) {
+    return number < 30;
+    // check if number is less than 30
+
+}
+// result = isLessThan30(11);
+// console.log(result);
+//or
+
+// Test whether number is Less than 30
+console.log("Result - ", isLessThan30(31))
